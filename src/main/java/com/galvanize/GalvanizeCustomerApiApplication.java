9a029908c532e72e7ded2ded0d5ce88ac25eb6e7
Kevin Peery <kevin.peery@gmail.com>
2020-03-17 20:08:32 -0500

    initial commit

---
+++ src/main/java/com/galvanize/GalvanizeCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalvanizeCustomerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalvanizeCustomerApiApplication.class, args);
+    }
+
+}
